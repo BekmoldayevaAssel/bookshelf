27b6d2cb09b6a6cafcfcf09800ea4f10cedae4e7
BekmoldayevaAssel <asellek82@mail.ru>
2022-06-03 21:02:50 +0600

    New

--- bookshell/admin.py
+++ bookshell/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import books, Authors, genres, company
 
 # Register your models here.
+admin.site.register(books)
+admin.site.register(Authors)
+admin.site.register(genres)
+admin.site.register(company)
--- bookshell/models.py
+++ bookshell/models.py
@@ -1,3 +1,5 @@
+from ast import Return
+from turtle import title
 from django.db import models
 
 # Create your models here.
@@ -8,11 +10,34 @@ class Authors(models.Model):
     data_of_birth = models.DateField()
     author_info = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.title
+
 class company(models.Model):
     title = models.CharField(max_length=100)
     country = models.CharField(max_length=50)
     language = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.title
+
+
 
 class genres(models.Model):
-     title = models.CharField(max_length=100)   
+     title = models.CharField(max_length=100) 
+
+     def __str__(self):
+        return self.title  
+
+class books(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('bookshell.Authors', on_delete=models.CASCADE)
+    company = models.ForeignKey('bookshell.company', on_delete=models.CASCADE)
+    genres = models.ForeignKey('bookshell.genres', on_delete=models.CASCADE)
+    year = models.IntegerField()
+    page = models.IntegerField()
+    book_info = models.CharField(max_length=300)
+   
+    def __str__(self):
+        return self.title
+
